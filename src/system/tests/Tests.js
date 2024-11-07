9a2f8b609b4cd1b00ba2058beb63a498f40892c6
Lee Crawford <lee.crawford@gmail.com>
2024-11-07 08:14:49 -0800

    Cleaned up the Tests page

--- src/system/tests/Tests.js
+++ src/system/tests/Tests.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 import Breadcrumb from "../../util/Breadcrumb";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import {TabPanel, wrap} from "../../util/Utils";
+import {observable} from "mobx";
+
+/**
+ *
+ */
 
 class Tests extends Component {
     static crumbs = [
@@ -8,59 +16,80 @@ class Tests extends Component {
         { label: "Tests" },
     ];
 
+    store = observable ({
+        tab: 0
+    });
+
     render() {
+        const { tab } = this.store;
+
         return (
             <div>
                 <Breadcrumb crumbs={Tests.crumbs} />
                 <h1>Tests</h1>
 
+                <Tabs value={tab} onChange={(e, nt) => this.store.tab = nt}>
+                    <Tab label={"UX"} />
+                    <Tab label={"Data"} />
+                    <Tab label={"System"} />
+                </Tabs>
+                <TabPanel value={tab} index={0}>
+                    <ul>
+                        <li>
+                            <a href={"#/tests/loading"}>
+                                Loading
+                            </a>
+                        </li>
+                        <li>
+                            <a href={"#/tests/propertyTable"}>
+                                PropertyTable
+                            </a>
+                        </li>
+                    </ul>
+                </TabPanel>
+                <TabPanel value={tab} index={1}>
+                    <ul>
+                        <li>
+                            <a href={"#/tests/senderPicker"}>
+                                SenderPicker
+                            </a>
+                        </li>
+                        <li>
+                            <a href={"#/tests/senderLink"}>
+                                SenderLink
+                            </a>
+                        </li>
+                        <li>
+                            <a href={"#/tests/userPicker"}>
+                                UserPicker
+                            </a>
+                        </li>
+                        <li>
+                            <a href={"#/tests/userLink"}>
+                                UserLink
+                            </a>
+                        </li>
+                        <li>
+                            <a href={"#/tests/templatePicker"}>
+                                TemplatePickerTest
+                            </a>
+                        </li>
+                    </ul>
+                </TabPanel>
+            <TabPanel value={tab} index={2}>
                 <ul>
-                    <li>
-                        <a href={"#/tests/loading"}>
-                            Loading
-                        </a>
-                    </li>
-                    <li>
-                        <a href={"#/tests/propertyTable"}>
-                            PropertyTable
-                        </a>
-                    </li>
-                    <li>
-                        <a href={"#/tests/senderPicker"}>
-                            SenderPicker
-                        </a>
-                    </li>
-                    <li>
-                        <a href={"#/tests/senderLink"}>
-                            SenderLink
-                        </a>
-                    </li>
-                    <li>
-                        <a href={"#/tests/userPicker"}>
-                            UserPicker
-                        </a>
-                    </li>
-                    <li>
-                        <a href={"#/tests/userLink"}>
-                            UserLink
-                        </a>
-                    </li>
                     <li>
                         <a href={"#/tests/template"}>
                             TemplateTest
                         </a>
                     </li>
-                    <li>
-                        <a href={"#/tests/templatePicker"}>
-                            TemplatePickerTest
-                        </a>
-                    </li>
                 </ul>
+            </TabPanel>
             </div>
         );
     }
 }
 
-export default Tests;
+export default wrap(Tests);
 
 // EOF
